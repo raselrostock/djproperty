13c8dcc6be91f6b17d8ed9660faa92c8cd48903c
raselrostock <raselrostock@gmail.com>
2020-10-07 17:47:32 +0600

    :seedling: Add: Added sitemaps

---
+++ djproperty/sitemaps/sitemap.py
@@ -0,0 +1,41 @@
+from django.contrib.sitemaps import Sitemap
+from django.contrib import sitemaps
+from django.shortcuts import reverse
+
+from listings.models import Listing
+from realtors.models import Realtor
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changfreq = 'daily'
+
+    def items(self):
+        return ['pages:about']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class ListingSitemap(Sitemap):
+    changfreq = 'daily'
+    priority = 0.9
+
+    def items(self):
+        return Listing.objects.all().order_by('-list_date')
+
+    def location(self, item):
+        # return reverse('courses:course-detail', args=[item.slug])
+        return reverse('listings:listing', kwargs={"listing_id": item.pk})
+
+
+# class RealtorSitemap(Sitemap):
+#     changfreq = 'daily'
+#     priority = 0.9
+
+#     def items(self):
+#         return Realtor.objects.all().order_by('-hire_date')
+
+#     def location(self, item):
+#         # return reverse('realtors:course-detail', args=[item.slug])
+#         return reverse('instructors:instructor-detail', kwargs={"instructor_name": item.username})
